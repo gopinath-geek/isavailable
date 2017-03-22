f92741cebdc5fcb8b5624bfb9cb6baeae7cd03d3
gopinath-geek <gopinath.geek@outlook.com>
2017-03-22 23:09:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 
-curl -X PUT -d '{ "first": "Jack", "last": "Sparrow" }' \
-  'https://firstdb-91a8e.firebaseio.com'
+$db = pg_connect("host=ec2-176-34-113-15.eu-west-1.compute.amazonaws.com port=5432 dbname=d7idvta5j12bu8 user=hbvbxoabwlcwwo password=7fc101a7a3462d275bde95493f6b66a35a17ec874b9a99b5eff263c56b4caabc");
+echo pg_connection_status($db);

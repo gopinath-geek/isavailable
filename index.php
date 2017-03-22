83561bc67a438dcdb360f02ee50621651aa7f64c
gopinath-geek <gopinath.geek@outlook.com>
2017-03-23 00:32:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,17 @@ $result = pg_query($db, $sql);
 
 $ip_address = getenv('HTTP_CLIENT_IP')?:getenv('HTTP_X_FORWARDED_FOR')?:getenv('HTTP_X_FORWARDED')?:getenv('HTTP_FORWARDED_FOR')?:getenv('HTTP_FORWARDED')?:getenv('REMOTE_ADDR');
 
+if(isset($_REQUEST['status']) && !empty($_REQUEST['status'])){
+      $query_string = $_REQUEST['status'];
+      if ($query_string == "Occupy"){
+            $sql = 'insert into isavailable values('.$ip_address.', 1)';
+            $result = pg_query($sql);
+            
+      }else if ($query_string == "Wait"){
+            
+      }
+}
+
 $sql = 'select count(*) as is_ip_exist from isavailable where ip_address='.$ipaddress;
 $result = pg_query($sql);
 $result_set = pg_fetch_all($sql);
@@ -25,11 +36,4 @@ if(count($result_set) == 0){
        $status_message = "Wait";
       echo '<div class="col-xs-5 col-xs-push-5"><form action=""><button type="submit" class="btn btn-danger" name="status" value="'.$status_message.'">'.$status_message.'</button></form></div>';
 }
-
-
-/*
-$selectfields = array("ip_address" => $ip_address);
-    $records = pg_select($db,"isavailable",$selectfields);
-    print_r($records);
-*/
 pg_close($db);

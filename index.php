5f74d9f3314c0896438784b44819081ec697d014
gopinath-geek <gopinath.geek@outlook.com>
2017-03-22 23:12:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 
-$db = pg_connect("host=ec2-176-34-113-15.eu-west-1.compute.amazonaws.com port=5432 dbname=d7idvta5j12bu8 user=hbvbxoabwlcwwo password=7fc101a7a3462d275bde95493f6b66a35a17ec874b9a99b5eff263c56b4caabc");
-echo pg_connection_status($db);
+$db = pg_connect("host=ec2-176-34-113-15.eu-west-1.compute.amazonaws.com port=5432 dbname=d7idvta5j12bu8 user=hbvbxoabwlcwwo password=7fc101a7a3462d275bde95493f6b66a35a17ec874b9a99b5eff263c56b4caabc") or exit("cannot connect db");
+$stat = pg_connection_status($db);
+
+if ($stat === PGSQL_CONNECTION_OK) {
+      echo 'Connection status ok';
+  } else {
+      echo 'Connection status bad';
+  }

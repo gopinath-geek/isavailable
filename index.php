e0b5513de2a88e2cbb94cf9057c75fbc4784adf1
gopinath-geek <gopinath.geek@outlook.com>
2017-03-22 23:42:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 $db = pg_connect("host=ec2-176-34-113-15.eu-west-1.compute.amazonaws.com port=5432 dbname=d7idvta5j12bu8 user=hbvbxoabwlcwwo password=7fc101a7a3462d275bde95493f6b66a35a17ec874b9a99b5eff263c56b4caabc") or exit("cannot connect db");
 $stat = pg_connection_status($db);
 
@@ -9,24 +9,13 @@ if ($stat === PGSQL_CONNECTION_OK) {
       echo 'Connection status bad';
   }
 
-$create_query = pg_prepare($db, "create_table", 'create table if not exist isavailable(ip_address varchar(50) primary key, status varchar(1) not null)');
-$create_query = pg_exec($db, "create_table", $create_query, array(""));
+$create_query = 'create table if not exist isavailable(ip_address varchar(50) primary key, status varchar(1) not null)';
+$create_query_exec = pg_query($db, "create_table");
 
-if($create_query == false){
-      echo "Db is down";
-      exit;
+if(!$create_query_exec){
+      echo pg_last_error($db);
 }else{
-      echo "Db works fine";
+      echo "Table created successfully";
 }
-*/
 
-  try {
-      $db = new PDO('pgsql:host=ec2-176-34-113-15.eu-west-1.compute.amazonaws.com;port=5432;dbname=d7idvta5j12bu8;user=hbvbxoabwlcwwo;password=7fc101a7a3462d275bde95493f6b66a35a17ec874b9a99b5eff263c56b4caabc);
-
-      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-      $sql ='CREATE TABLE IF NOT EXISTS test (ip_address varchar(50) PRIMARY KEY, availability VARCHAR(1));';
-      $db->exec($sql);
-  } catch(PDOException $e) {
-      echo $e->getMessage();
-  }
+pg_close($db);

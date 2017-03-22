86f4b4c1dcc208d9c9895db1fab86cd82e4b5302
gopinath-geek <gopinath.geek@outlook.com>
2017-03-23 00:20:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,8 +14,20 @@ $result = pg_query($db, $sql);
 
 $ip_address = getenv('HTTP_CLIENT_IP')?:getenv('HTTP_X_FORWARDED_FOR')?:getenv('HTTP_X_FORWARDED')?:getenv('HTTP_FORWARDED_FOR')?:getenv('HTTP_FORWARDED')?:getenv('REMOTE_ADDR');
 
+$sql = 'select count(*) as is_ip_exist from isavailable where ip_address='.$ipaddress;
+$result = pg_query($sql);
+$result_set = pg_fetch_all($sql);
+$status_message = "Occupy";
+if(count($result_set) == 0){
+      echo '<div class="col-xs-5 col-xs-push-5"><form action=""><button type="submit" class="btn btn-primary" name="status">'.$status_message.'</button></form></div>';
+}else{
+      
+}
+
+
+/*
 $selectfields = array("ip_address" => $ip_address);
     $records = pg_select($db,"isavailable",$selectfields);
     print_r($records);
-
+*/
 pg_close($db);

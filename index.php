892c03c7fd916885cd4680c71037a5c0c9be91fe
gopinath-geek <gopinath.geek@outlook.com>
2017-03-23 16:46:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -83,6 +83,7 @@ function get_ip_from_db(){
 }
 
 function truncate_table(){
+      $db = pg_connect("host=ec2-176-34-113-15.eu-west-1.compute.amazonaws.com port=5432 dbname=d7idvta5j12bu8 user=hbvbxoabwlcwwo password=7fc101a7a3462d275bde95493f6b66a35a17ec874b9a99b5eff263c56b4caabc") or exit("cannot connect db");
       $sql = "truncate table isavailable";
       $result = pg_query($db, $sql);
 }

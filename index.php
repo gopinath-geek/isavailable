d8cdbf23de3c7284a14142d80e067c3e4a8eb797
gopinath-geek <gopinath.geek@outlook.com>
2017-03-23 19:30:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -126,6 +126,19 @@ function truncate_table(){
       $sql = "truncate table isavailable";
       $result = pg_query($db, $sql);
 }
+
+function flush_session(){
+      $db = pg_connect("host=ec2-176-34-113-15.eu-west-1.compute.amazonaws.com port=5432 dbname=d7idvta5j12bu8 user=hbvbxoabwlcwwo password=7fc101a7a3462d275bde95493f6b66a35a17ec874b9a99b5eff263c56b4caabc") or exit("cannot connect db");
+      $sql = 'select timestamp from isavailable';
+      $result = pg_query($db, $sql);
+      $result_set = pg_fetch_all($result);
+      $timestamp = $result_set[0]["timestamp"];
+      
+      if($timestamp == time()-600){
+            truncate_table();
+      }
+}
+       
 pg_close($db);
 ?>
 </body>

fd1875e635e3a64bde6e05067659d89c0eb59af5
gopinath-geek <gopinath.geek@outlook.com>
2017-03-23 18:09:21 +0530

    Create default.php

---
+++ default.php
@@ -0,0 +1,8 @@
+<?php
+
+echo getenv('HTTP_CLIENT_IP')."\n";
+echo getenv('HTTP_X_FORWARDED_FOR')."\n";
+echo getenv('HTTP_X_FORWARDED')."\n";
+echo getenv('HTTP_FORWARDED_FOR')."\n";
+echo getenv('HTTP_FORWARDED')."\n";
+echo getenv('REMOTE_ADDR')."\n";

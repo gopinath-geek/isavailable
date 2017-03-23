864249a116bd8061986df61b43b9e3216a07cbcf
gopinath-geek <gopinath.geek@outlook.com>
2017-03-23 18:16:10 +0530

    Update default.php

--- default.php
+++ default.php
@@ -6,3 +6,5 @@ echo "three ".getenv('HTTP_X_FORWARDED')."\n";
 echo "four ".getenv('HTTP_FORWARDED_FOR')."\n";
 echo "five ".getenv('HTTP_FORWARDED')."\n";
 echo "six ".getenv('REMOTE_ADDR')."\n";
+
+print_r($_SERVER['HTTP_USER_AGENT']);
